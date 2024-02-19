44521c5f44935c2c6652a518a337c8a752e1d273
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-19 23:41:52 +0200

    Update gallery.tsx ho
    Update gallery.tsx ho

--- components/gallery/gallery.tsx
+++ components/gallery/gallery.tsx
@@ -28,16 +28,7 @@ const Gallery = () => {
             sizes='60vw' priority
           />
          
-          <Image
-            src="https://res.cloudinary.com/diymc30tp/image/upload/v1708364944/rectangle-8-bg-1dF_zeatvc.jpg"
-            alt=""
-            layout="responsive"
-            width={100}
-            height={100}
-            className="rounded-lg nowimage"
-            style={{width:'95%', height: '100%', objectFit: 'contain'}}
-            sizes='60vw' priority
-          />
+        
       
           <Image
             src="/rectangle-15-38Z.png"
