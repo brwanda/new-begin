3c794b2969300df7d6bbedf2784734ece54771a5
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-19 23:41:07 +0200

    Update gallery.tsx

--- components/about-us/gallery.tsx
+++ components/about-us/gallery.tsx
@@ -27,16 +27,7 @@ const Gallery = () => {
             sizes='60vw' priority
           />
          
-          <Image
-            src="https://res.cloudinary.com/diymc30tp/image/upload/v1708364314/cld-sample-2.jpg"
-            alt=""
-            layout="responsive"
-            width={100}
-            height={100}
-            className="rounded-lg nowimage"
-            style={{width:'95%', height: '100%', objectFit: 'contain'}}
-            sizes='60vw' priority
-          />
+       
       
           <Image
             src="/rectangle-15-38Z.png"
