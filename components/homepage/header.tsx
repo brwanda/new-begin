ff690a1cf52ecf3dcdd89f6bc097217d3451adc9
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-21 01:29:35 +0200

    Update header.tsx header

--- components/homepage/header.tsx
+++ components/homepage/header.tsx
@@ -13,7 +13,7 @@ export default function Home() {
    <div
    className="bg-cover bg-center headerimag"
    style={{
-     backgroundImage: "url('https://res.cloudinary.com/dxtjjbk95/image/upload/v1708471436/Authentic%20Events/53437797093_e34282213d_o_kvkqxr.jpg')",
+     backgroundImage: "url('https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470192/Authentic%20Events/53437994064_96b0fbc78d_o_qvss2a.jpg')",
      height: "100vh auto",
    }}>
 <div className="container mx-auto px-5 py-4 flex justify-between items-center">
