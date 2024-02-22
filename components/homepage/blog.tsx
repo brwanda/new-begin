2beb00d3753531457570183aa6a029188f25dacd
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-22 14:49:49 +0200

    Update blog.tsx auto play

--- components/homepage/blog.tsx
+++ components/homepage/blog.tsx
@@ -10,6 +10,10 @@ import Image from "next/image"
 
 
 export default function Component() {
+     const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  )
+ 
   return (
     <div className="bg-white py-12">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -28,7 +32,10 @@ export default function Component() {
           </div>
         </div>
         <div className="mt-10">
-        <Carousel>
+        <Carousel  plugins={[plugin.current]}
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+            >
       <CarouselContent className="-ml-1">
       <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3 flex flex-col rounded-lg shadow-lg overflow-hidden">
       <div className="p-1">
